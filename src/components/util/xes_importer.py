20d4057713807346f8095d52c8d8cf8de09a481c
Bohne9 <jonah@die-schuellers.de>
2020-11-23 14:33:48 +0100

    🚨 Fixed all linter warnings in xes_importer.py .

--- src/components/util/xes_importer.py
+++ src/components/util/xes_importer.py
@@ -1,35 +1,38 @@
+"""Module for importing XES event logs in various formats."""
 from pm4py.objects.log.importer.xes import importer as xes_importer
 from tempfile import NamedTemporaryFile
-from flask import request
+
 
 class XES_Importer:
+    """Importer class for importing XES event logs.
 
+    The supported types are files, strings and HTTP requests.
+    """
 
     def __save_to_temp_file(self, content: str):
-
+        """Save a given string to a temporary file."""
         # Create a temporary file that won't get deleted
         file = NamedTemporaryFile(delete=False)
-        
+
         with open(file.name, 'w') as f:
             f.write(content)
 
         return file
 
-
     def import_file(self, path: str):
+        """Import XES event logs from a given file."""
         log = xes_importer.apply(path)
 
         return log
 
-
     def import_str(self, event_log: str):
-
+        """Import XES event logs from a given string."""
         file = self.__save_to_temp_file(event_log)
 
         return self.import_file(file.name)
 
-
     def import_http_query(self, request):
+        """Import XES event logs from a given HTTP request."""
         data = request.data
 
         return self.import_str(data)
