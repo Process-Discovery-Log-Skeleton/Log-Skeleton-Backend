d26a3ffa6c0e5872d9703fa1f72e6630a584fa25
Bohne9 <jonah@die-schuellers.de>
2020-11-24 13:14:25 +0100

    🐛 Fixed the API routes.

--- src/api/server.py
+++ src/api/server.py
@@ -4,10 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('log-skeleton', methods=['GET'])
+@app.route('/log-skeleton', methods=['GET'])
 def log_skeleton():
     """Provide endpoint at /log-skeleton."""
-    return 'Not implemented yet.'
+    return 'Warning! The API is not implemented yet.'
 
 
 app.run()
