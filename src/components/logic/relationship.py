b1ad4b26a48e6f200b8ff2c0ce7f559b3630076a
Bohne9 <jonah@die-schuellers.de>
2020-11-25 12:35:43 +0100

    🐛 Fixed Relationship base + never_together implementation + Test adaptation.

--- src/components/logic/relationship.py
+++ src/components/logic/relationship.py
@@ -11,6 +11,7 @@ __CONCEPT_NAME__ = 'concept:name'
 TRACE_START = {__CONCEPT_NAME__: uuid.uuid4().hex}
 TRACE_END = {__CONCEPT_NAME__: uuid.uuid4().hex}
 
+
 class Relationship:
     """Base class for the log-skeleton relationship implementations.
 
@@ -19,36 +20,54 @@ class Relationship:
     implementation of the relationship algorithm as easy as possible.
     """
 
-    def __init__(self, log, extended=False):
+    def __init__(self, log):
         """Store the traces."""
         self.log = log
+        self.activities = self.extract_activities()
 
-        if extended:
-            self.log = map(lambda trace: self.extended_trace(trace), log)
+    def extract_activities(self):
+        """Extract the activity set from the log."""
+        activities = set()
+
+        for trace in self.log:
+            for activity in trace:
+                activities.add(self.activity_concept_name(activity))
+
+        print(activities)
+        return activities
 
     def extended_trace(self, trace):
-        """Convert a trace to the extended trace"""
+        """Convert a trace to the extended trace."""
         return [TRACE_START] + trace + [TRACE_END]
 
     # Activity related functions
-    def activity_concept_name(self, activity):
+    def activity_concept_name(self, activity) -> str:
         """Extract the concept:name of an activity."""
         return activity[__CONCEPT_NAME__]
 
+    def is_start(self, activity) -> bool:
+        """Determine whether an activity is the start activity."""
+        return activity == self.activity_concept_name(TRACE_START)
+
+    def is_end(self, activity) -> bool:
+        """Determine whether an activity is the end activity."""
+        return activity == self.activity_concept_name(TRACE_END)
+
+    def is_extension_activity(self, activity) -> bool:
+        """Determine whether an activity is one of the extension activities."""
+        return self.is_start(activity) or self.is_end(activity)
+
     # Trace related functions
-    def is_empty(self, trace):
+    def is_empty(self, trace) -> bool:
         """Return whether the trace is empty."""
         return len(trace) == 0
 
     def project_trace(self, trace, elements):
         """Project the trace to a given set of activities."""
-        activity_names = list(
-            map(lambda ac: self.activity_concept_name(ac), elements))
-
         res = filter(
-            lambda ac: self.activity_concept_name(ac) in activity_names, trace)
+            lambda ac: self.activity_concept_name(ac) in elements, trace)
 
-        return list(res)
+        return list(map(lambda ac: self.activity_concept_name(ac), res))
 
     def first(self, trace):
         """Return the first activity."""
@@ -66,37 +85,44 @@ class Relationship:
         """Implement a relationship algorithm."""
         results = []
 
-        for trace in self.log:
-            res = self.apply_to_trace(trace)
+        # trace = [a, b, c]
+        # trace x trace = [(a, a), (a, b), ..., (c, a), (c, b), (c, c)]
+        source = itertools.product(self.activities, self.activities)
 
-            results.append(res)
+        print(self.log)
+        for a1, a2 in source:
+            res = True
+            for trace in self.log:
+                print('trace')
+                res = res and self.apply_to_trace(trace, a1, a2)
+
+            if res:
+                results.append((a1, a2))
 
         # Flatten out the list
         # Each apply_to_trace returns a list of results.
         #   results might look like -> [[(a, b), (b, c)], [(b, d)], [(c, a)]]
         # Flattening this example leads to: [(a, b), (b, c), (b, d), (c, a)]
-        flattenedResult = [val for traceRes in results for val in traceRes]
+        # flattenedResult = [val for traceRes in results for val in traceRes]
 
-        return flattenedResult
+        return results
 
-    def apply_to_trace(self, trace):
+    def apply_to_trace(self, trace, a1, a2) -> bool:
         """Apply the matching algorithm to each pair of activities."""
+        if self.activity_pair_matches(trace, a1, a2):
 
-        # trace = [a, b, c]
-        # trace x trace = [(a, a), (a, b), ..., (c, a), (c, b), (c, c)]
-        source = list(itertools.product(trace, trace))
+            if self.is_extension_activity(a1) \
+                    or self.is_extension_activity(a2) or a1 == a2:
+                return False
 
-        result = []
+            return True
 
-        for a1, a2 in source:
-            if self.activity_pair_matches(a1, a2):
-                result.append((a1, a2))
-        
-        return result
+        return False
 
-    def activity_pair_matches(self, activity1, activity2):
+    def activity_pair_matches(self, activity1, activity2) -> bool:
         """Determine if the given pair of activities is in the result."""
         raise NotImplementedError
+
 
 if __name__ == "__main__":
     importer = XES_Importer()
--- tests/relationship_test.py
+++ tests/relationship_test.py
@@ -24,7 +24,7 @@ traces = [
 ]
 
 
-rel = Relationship(traces=traces)
+rel = Relationship(traces)
 
 
 def test_activity_concept_name():
@@ -34,19 +34,20 @@ def test_activity_concept_name():
 
 def test_project_trace():
     """Test the trace projection function."""
-    projection = rel.project_trace(traces[0], [traces[0][0]])
+    projection = rel.project_trace(
+        traces[0], [rel.activity_concept_name(traces[0][0])])
 
     # Test the first item
-    assert rel.activity_concept_name(projection[0]) == 'Test1.1'
+    assert projection[0] == 'Test1.1'
 
     projection = rel.project_trace(traces[0], [])
 
     # Test the empty projection
     assert len(projection) == 0
 
-    projection = rel.project_trace(traces[0], traces[0])
+    projection = rel.project_trace(traces[0], rel.activities)
 
-    assert projection == traces[0]
+    assert projection == ['Test1.1', 'Test1.2', 'Test1.3']
 
 
 def test_first():
