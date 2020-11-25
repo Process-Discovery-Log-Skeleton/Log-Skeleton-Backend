e056c52d2fd363ca8b673335b807e22695bbf8fc
Bohne9 <jonah@die-schuellers.de>
2020-11-25 08:13:41 +0100

    🚨 Fix all linter warnings.

--- src/components/logic/relationship.py
+++ src/components/logic/relationship.py
@@ -1,55 +1,53 @@
+"""Base class for the log-skeleton relationship implementations."""
 # XES-concept extension. General identifier field of an event.
 __CONCEPT_NAME__ = 'concept:name'
 
 from src.components.util.xes_importer import XES_Importer
 
+
 class Relationship:
+    """Base class for the log-skeleton relationship implementations.
+
+    This class can be inhertited to implement the relationship algorithms.
+    It provides different kinds of helper functions to make the
+    implementation of the relationship algorithm as easy as possible.
+    """
 
     def __init__(self, traces):
+        """Store the traces."""
         self.traces = traces
-
 
     # Activity related functions
     def activity_concept_name(self, activity):
-        """
-        Extract the concept:name of an activity
-        """
+        """Extract the concept:name of an activity."""
         return activity[__CONCEPT_NAME__]
 
     # Trace related functions
 
     def project_trace(self, trace, elements):
-        """
-        Project the trace to a given set of activities.
-        """
-        activity_names = list(map(lambda ac: self.activity_concept_name(ac), elements))
+        """Project the trace to a given set of activities."""
+        activity_names = list(
+            map(lambda ac: self.activity_concept_name(ac), elements))
 
-        res = filter(lambda ac: self.activity_concept_name(ac) in activity_names, trace)
+        res = filter(
+            lambda ac: self.activity_concept_name(ac) in activity_names, trace)
 
         return list(res)
 
     def first(self, trace):
-        """
-        Return the first activity.
-        """
+        """Return the first activity."""
         return trace[0]
 
     def last(self, trace):
-        """
-        Return the last activity.
-        """
+        """Return the last activity."""
         return trace[-1]
 
     def count(self, trace):
-        """
-        Return the number of activity in that one trace.
-        """
+        """Return the number of activity in that one trace."""
         return len(trace)
 
     def apply(self):
-        """
-        Implements one relationship algorithm.
-        """
+        """Implement a relationship algorithm."""
         result = []
 
         for trace in self.traces:
@@ -60,14 +58,11 @@ class Relationship:
         return result
 
     def apply_to_trace(self, trace):
-        """
-        Apply a certain algorithm to a spectific trace.
-        """
+        """Apply a certain algorithm to a spectific trace."""
         raise NotImplementedError
 
 
 if __name__ == "__main__":
-    
     importer = XES_Importer()
 
     log = importer.import_file('../../../res/logs/running-example.xes')
--- tests/relationship_test.py
+++ tests/relationship_test.py
@@ -1,3 +1,4 @@
+"""Test cases for the base relationship class."""
 from src.components.logic.relationship import Relationship
 
 traces = [
@@ -22,18 +23,17 @@ traces = [
     ]
 ]
 
+
 rel = Relationship(traces=traces)
 
+
 def test_activity_concept_name():
-    """
-    Test the activity concept name extractor.
-    """
+    """Test the activity concept name extractor."""
     assert rel.activity_concept_name(traces[0][0])
 
+
 def test_project_trace():
-    """
-    Test the trace projection function.
-    """
+    """Test the trace projection function."""
     projection = rel.project_trace(traces[0], [traces[0][0]])
 
     # Test the first item
@@ -48,20 +48,17 @@ def test_project_trace():
 
     assert projection == traces[0]
 
+
 def test_first():
-    """
-    Test the first function of the relationship base class.
-    """
-    assert rel.first(traces[0]) == { 'concept:name': 'Test1.1' }
+    """Test the first function of the relationship base class."""
+    assert rel.first(traces[0]) == {'concept:name': 'Test1.1'}
+
 
 def test_last():
-    """
-    Test the last function of the relationship base class.
-    """
-    assert rel.first(traces[0]) == { 'concept:name': 'Test1.3' }
+    """Test the last function of the relationship base class."""
+    assert rel.last(traces[0]) == {'concept:name': 'Test1.3'}
+
 
 def test_count():
-    """
-    Test the count function of the relationship base class.
-    """
-    assert rel.first(traces[0]) == { 'concept:name': 'Test1.3' }
+    """Test the count function of the relationship base class."""
+    assert rel.count(traces[0]) == 3
